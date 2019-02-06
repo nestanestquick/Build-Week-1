1b18a59aee0ba789a844ef605aa4e31cbbfc1d95
Beniamin Vysotski <nestaphysics@gmail.com>
2019-02-06 14:44:52 -0900

    added menu tab and fixed some redundancies

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,15 @@
-
+//Lohin button changes color
 document.querySelectorAll('nav a').forEach(function(link){link.addEventListener("mouseover", function(e){
     e.target.style.color = "purple"; setTimeout( () => {e.target.style.color ="";}, 500);}, false)
 });
+
+// tsting
+
+
+const toggleMenu = () => {
+    menu.classList.toggle('login--open');
+  }
+  const menu = document.querySelector(".login");
+  const menuButton = document.querySelector(".login-tab");
+  menuButton.addEventListener("click", toggleMenu);
+  
