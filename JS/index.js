b3e0946bd3304d5ac0755e2eeaba599b8349bcb5
Beniamin Vysotski <nestaphysics@gmail.com>
2019-02-08 10:31:00 -0900

    pass pls

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,27 @@
-//Lohin button changes color
+//Login button changes color
 document.querySelectorAll('nav a').forEach(function(link){link.addEventListener("mouseover", function(e){
     e.target.style.color = "purple"; setTimeout( () => {e.target.style.color ="";}, 500);}, false)
 });
 
-// tsting
-
-
+// testing
 const toggleMenu = () => {
     menu.classList.toggle('login--open');
   }
   const menu = document.querySelector(".login");
   const menuButton = document.querySelector(".login-tab");
   menuButton.addEventListener("click", toggleMenu);
+
+//exp tabs
+class Tab {
+    constructor(element) {
+        this.element = element; 
+        this.openButton = element.querySelectorAll('.openButton');
+        this.openButton.forEach((item) => item.textContent = 'Open Sesame');
+        this.openButton.forEach((item) => item.addEventListener('click', () => this.expandTabs()));
+    }
+    expandTabs() {
+        this.element.classList.toggle('article-open');
+    }
+  }  
+let plswork = document.querySelectorAll('.openSesame').forEach(tab => new Tab(tab)); 
   
---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+This is the landing page for a Rural schools resource managment site/app Build Week UI project
+In Collaboration with IOS/Back End, and Adv Front End classes
+
+group project deployed at https://keen-mccarthy-75ca2a.netlify.com/
